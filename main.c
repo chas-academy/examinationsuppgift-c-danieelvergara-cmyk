18cec0626623fe75908d6f700029dff3644b75da
danieelvergara-cmyk <danieel.vergara@gmail.com>
2026-05-19 21:34:39 +0200

    Add initial comments and include headers in main.c

--- main.c
+++ main.c
@@ -1,2 +1,11 @@
-//Write your code in this file
+/* Examinationsuppgift - Studentresultat */
+/* Lasas in 5 elever med 13 provresultat vardera */
 
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main() {
+
+    return 0;
+}
